366c8bbc05392fa90e9c32195c1444690816bb17
D <danixoon@gmail.com>
2019-04-28 18:46:26 +0300

    heroku-test

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ import account from "./routes/api/account";
 import user from "./routes/api/user";
 
 import test from "./initDatabase";
+import * as fs from "fs";
 
 const app = express();
 
@@ -21,23 +22,24 @@ app.use("/api/job", job);
 app.use("/api/account", account);
 app.use("/api/user", user);
 
-
 //Static file declaration
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 //production mode
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === "production") {
+  if (!process.env.MONGO_URI || !process.env.JWT_SECRET) throw "ENV VARIABLES INCORRECT";
+  fs.writeFileSync("../config/default.json", { MongoURI: process.env.MONGO_URI, jwtSecret: process.env.JWT_SECRET });
+
+  app.use(express.static(path.join(__dirname, "client/build")));
   //
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'client/build/index.html'));
-  })
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
+  });
 }
 //build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
-})
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+});
 
 const db = config.get("MongoURI") as string;
 
@@ -53,5 +55,5 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, async () => {
   console.log(`Server listening at ${port} port`);
-  await test();
+  // await test();
 });
